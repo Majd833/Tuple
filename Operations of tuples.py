6ab0ce7b5070494a1fa22abbf0add29fbfc94040
Majd833 <lina.azzam.1611@gmail.com>
2024-11-07 17:47:41 +0200

    Operations of Tuples

---
+++ Operations of tuples.py
@@ -0,0 +1,14 @@
+tuplex=(2,9.2,False,"code")
+print(tuplex)
+tuplex=(2,6,4,9,6,7)
+# create a new tuple 
+tuplex=tuplex + (5,)
+print(tuplex)
+tuple1=(20,38,46,20,55)
+count=(tuple1.count(20))
+tuple2=(2,9,5,7,6,3,1,5,87,4,9,1,4,3,6)
+_slice=tuple2[3:6]
+_slices=tuple2[:12]
+print(count)
+print(_slice)
+print(_slices)
